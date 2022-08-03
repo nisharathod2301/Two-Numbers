98bdfb9b941ddf4d8fc73928f23051634f52c172
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2022-08-03 18:48:59 +0530

    Update TwoNumbers.py

--- TwoNumbers.py
+++ TwoNumbers.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {} # val : index
         for i, n in enumerate(nums):
-            if n<=target:
+            #if n<=target:  Use this if input is only positive integers
                 diff = target-n
                 if diff in hashmap:
                     return [hashmap[diff],i]
